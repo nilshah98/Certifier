349b3e4c7dc81d36cbc411f073091e179a58e2a3
nurdtechie98 <nurdtechie98@gmail.com>
2019-02-27 21:09:38 +0530

    modify ipfs for intermediate file

--- ipfs.js
+++ ipfs.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 //Connceting to the ipfs network via infura gateway
 const ipfs = ipfsAPI({host: 'localhost', port: '5001', protocol: 'http'})
 
-const upload = (filepath)=>{ 
+
+
+const upload_doc = (filepath)=>{ 
     //Reading file from computer
     let testFile = fs.readFileSync(filepath);
     //Creating buffer for ipfs function to add file to the system
@@ -21,6 +23,23 @@ const upload = (filepath)=>{
     });
 }
 
+const upload_intermediate = (intermediate)=>{ 
+    //Creating buffer for ipfs function to add file to the system
+    let testBuffer = new Buffer(JSON.stringify(intermediate));
+
+    //Addfile router for adding file a local file to the IPFS network without any local node
+    //app.get('/addfile', function(req, res) {
+
+    ipfs.add(testBuffer, function (err, file) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(file)
+    });
+}
+
+//upload_intermediate("testing1234","qawsedcvfraz","this is a test documents intermediate",1);
+
 //This hash is returned hash of addFile router.
 const validCID = 'HASH_CODE';
 
@@ -33,3 +52,17 @@ const getFile = (validCID)=>{
         })
     })
 }
+
+const get_json = (validCID)=>{
+    ipfs.cat(validCID, function (err, file) {
+            const json_obj = JSON.parse(file);
+            return json_obj;
+    })
+}
+
+get_json("Qmb6eEURAKZaTKqqhkAgUdHwCUa7QtxGxk6Yc9PTtu8zqx");
+
+
+const update_access = (validCID)
+
+//getFile("QmSUV9cyBRU5f2h14FNema49suhGwLrF4QqCKtbWnLmMhK");
