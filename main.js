50317a79237cc0168aa4d7b0cd6e776eb9c1c991
nurdtechie98 <nurdtechie98@gmail.com>
2019-03-01 23:41:47 +0530

    add new methods to main.js

--- main.js
+++ main.js
@@ -173,9 +173,13 @@ window.onload = async () => {
 // First make a contract instance for series. 
 // Series name randomly generated.
 
+var new_contract = (series_address) => {
+    newcontract = new web3.eth.Contract(abi2,series_address);
+}
+
 var addcontract = async (arr1, arr2) => {
-    var series_name = "abcd";  //random generation
-    await contract.methods.issue(series_name).send(
+    //var series_name = "abcd";  //random generation
+    await contract.methods.issue().send(
         {
             from: accounts[0],
             gas: '4700000'
@@ -183,11 +187,6 @@ var addcontract = async (arr1, arr2) => {
     )
     var series_address = await get_contract_address(series_name);
     console.log(series_address);
-    
-    newcontract = new web3.eth.Contract(abi2,series_address);
-
-    console.log(newcontract);
-
     //  5 entries in CSV
     // for(var i=0;i<5;i++){
     //     generate ipfshash
@@ -197,7 +196,7 @@ var addcontract = async (arr1, arr2) => {
     // Convert csv into 2 arrays
     //var arr1 = [web3.utils.fromAscii("abcdefgh"),web3.utils.fromAscii("xyzwerqw")];
     //var arr2 = ["0xfa17349ef48e20b98539b54C769d2c0DE7e65880","0xfa17349ef48e20b98539b54C769d2c0DE7e65880"];
-    
+    new_contract(series_address);
     await addcertificate(arr1, arr2);
 
 };
@@ -221,3 +220,57 @@ var addcertificate = async (arr1, arr2) => {
         }
     )
 }
+
+var get_personal_ipfs = async(arr1,arr2) => {
+    var ipfs = await contract.methods.get_personal_ipfs().call(
+        {
+            from:accounts[0],
+            gas: '4700000'
+        }
+    )
+    return ipfs;
+}
+
+var update_personal_ipfs = async(ipfs_hash) => {
+    await contract.methdods.update_personal_ipfs(ipfs_hash).send(
+        {
+            from:accounts[0],
+            gas: '4700000'
+        }
+    )
+}
+
+var get_certificate = async(unique_id) => {
+    var series_name = unique_id.split("-")[0];
+    var serial_id = unique_id.split("-")[1];
+    var contract_address = await get_contract_address(series_name);
+    new_contract(contract_address);
+    var ipfs_hash1 = '' ; //get intermediate ipfs 
+    var ipfs_hash2 = '' ; //get intermediate ipfs 
+    var ipfs_hash = web3.utils.toAscii(ipfs_hash1)+web3.utils.toAscii(ipfs_hash2);
+    var result = get_json(ipfs_hash);
+    if(result.pubpriv == 0)
+        return result;
+    else
+    {
+        accessible = results.access;
+        if(accessible.include(accounts[0]))
+        {
+            return result;
+        }
+        else
+        {
+            return "Either the certificates doesn't exist or is private";
+        }
+    }
+}
+
+var claim_certificate = async(unique_id) => {
+    var series_name = unique_id.split("-")[0];
+    var serial_id = unique_id.split("-")[1];
+    var contract_address = await get_contract_address(series_name);
+    new_contract(contract_address);
+    checkOwner
+}
+
+
